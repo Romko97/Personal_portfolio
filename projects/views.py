cd0a2524148be5a94b3e8ae04a69713d54a96c32
Roman <elgoog6651@gmail.com>
2020-06-29 14:05:36 +0300

    add moduls for post and comments and categoreis

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from projects.models import Project
 
-def project_index():
+def project_index(request):
     projects = Project.objects.all()
-    context =   {"projects": projects}
-    return render( request, 'project_index.html', context)
+    context = {'projects': projects}
+    return render(request, 'project_index.html', context)
 
 def project_detail(request, pk):
-    project = Project.objects.get(pk=pk) # pk primary key, 
-    context = { "project":project}
-    return render ( request, 'project_detail', context)
+    project = Project.objects.get(pk=pk)
+    context = {'project': project}
+    return render(request, 'project_detail.html', context)
 
