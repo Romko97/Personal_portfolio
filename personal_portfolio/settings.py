32cb22bfe369c76f0f24253a19c39ace968f81ec
Roman <elgoog6651@gmail.com>
2020-08-09 21:18:49 +0300

    some change3

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 from django.conf import settings
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,4 +134,4 @@ STATICFILES_DIRS = (
 
 ALLOWED_HOSTS = ['gentle-ridge-07301.herokuapp.com', '127.0.0.1']
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
