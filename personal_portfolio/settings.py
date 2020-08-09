df6a72ad125fbfe046cbb2365611db906508c586
Roman <elgoog6651@gmail.com>
2020-08-09 21:00:09 +0300

    some change

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # I dont know what this is doing 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = '/static/'
 
