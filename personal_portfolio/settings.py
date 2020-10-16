f32dfcc215d808943a49c695535e9c051c43918d
Roman <elgoog6651@gmail.com>
2020-10-16 15:05:16 +0300

    change alternativ sentenses to photo and one more rule in settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
